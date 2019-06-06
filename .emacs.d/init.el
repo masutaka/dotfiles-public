1df95e4f92f2155d8b3823aedd621f392a075fdb
Takashi Masuda <masutaka.net@gmail.com>
2019-06-06 21:53:57 +0900

    Auto commit in CircleCI

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -431,7 +431,7 @@ bothが non-nilの場合は、両方のWindowがスクロールアップしま�
 
 (setq helm-esa-team-name "feedforce")
 (setq helm-esa-access-token (my-lisp-load "helm-esa-access-token"))
-(setq helm-esa-search-query "watched:true -in:日報")
+(setq helm-esa-search-query "watched:true kind:stock -category:Templates")
 (setq helm-esa-debug-mode t)
 (helm-esa-initialize)
 
