ff38c78426b1424d1dc9908a2a844d3f424ad790
Takashi Masuda <masutaka.net@gmail.com>
2023-01-12 04:06:48 +0000

    Auto commit in GitHub Actions

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,7 +3,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defconst machine-personal-p (equal "VivoBook" (system-name)) "Is this a personal computer?")
-(defconst machine-feedforce-p (string-match "^pc20" (system-name)) "Is this a feedforce computer?")
+(defconst machine-work-p (string-match "^pc20" (system-name)) "Is this a work computer?")
 
 (defconst os-linux-p (eq system-type 'gnu/linux) "Linux")
 (defconst os-mac-p (eq system-type 'darwin) "macOS")
