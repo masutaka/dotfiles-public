41cd8207e8ec11cbb3595e8cd674193e11c407ea
Takashi Masuda <masutaka.net@gmail.com>
2025-04-24 03:48:35 +0000

    Auto commit in GitHub Actions

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1426,6 +1426,7 @@ DO NOT SET VALUE MANUALLY.")
 (require 'web-mode)
 
 (defun web-mode-hook-func ()
+  (setq web-mode-code-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-markup-indent-offset 2)
   (setq indent-tabs-mode nil))
